cb35581063b24ce373a8ae8cb8ba6c3df553e1f0
Jacob Klimczak <jacob.klimczak@gmail.com>
2022-11-26 00:42:00 -0500

    add test of processing

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"github.com/Jacob4649/go-voxelize/go-voxelize/lasProcessing"
+	"github.com/Jacob4649/go-voxelize/go-voxelize/voxels"
+	"github.com/jblindsay/lidario"
+)
 
+func main() {
+	fileName := "DemoFile.las"
+
+	file, err := lidario.NewLasFile(fileName, "r")
+
+	if err != nil {
+		panic(err)
+	}
+
+	chunks := lasProcessing.ChunkFile(file, 36)
+
+	processor := voxels.VoxelSetProcessor{}
+
+	test := lasProcessing.ConcurrentProcess[voxels.VoxelSet](file, chunks, &processor, 8, 1)
+
+	if test == nil {
+		println("Error with test")
+	} else {
+		print("Success")
+	}
 }
