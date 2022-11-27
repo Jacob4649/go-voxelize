a761461ec4291f9628d10bb5790259bcd57ae1b3
Jacob Klimczak <jacob.klimczak@gmail.com>
2022-11-27 00:34:26 -0500

    configurable voxel size

--- main.go
+++ main.go
@@ -90,12 +90,13 @@ func writeToFile(voxels *voxels.VoxelSet, fileName string) error {
 // Main function
 func main() {
 
-	if len(os.Args) < 3 || len(os.Args) > 3 && len(os.Args) < 6 {
-		println("Usage: voxelize <input LAS path> <output CSV path> <optional chunk number> <optional concurrency> <optional density>")
-		println("Must specify chunk number, concurrency, and density to use any")
+	if len(os.Args) < 3 || len(os.Args) > 3 && len(os.Args) < 7 {
+		println("Usage: voxelize <input LAS path> <output CSV path> <optional chunk number> <optional concurrency> <optional density> <optional voxel size>")
+		println("Must specify chunk number, concurrency, density, and voxel size to use any")
 		println("Default chunk number: 256")
 		println("Default concurrency: 32")
 		println("Default density: 20")
+		println("Default voxel size: 0.1")
 		return
 	}
 
@@ -109,18 +110,21 @@ func main() {
 
 	density := 20
 
-	if len(os.Args) >= 6 {
+	voxelSize := 0.1
+
+	if len(os.Args) >= 7 {
 		chunkString := os.Args[3]
 		concurrencyString := os.Args[4]
 		densityString := os.Args[5]
+		voxelSizeString := os.Args[6]
 
-		var err error
-		chunkNumber, err = strconv.Atoi(chunkString)
-		concurrency, err = strconv.Atoi(concurrencyString)
-		density, err = strconv.Atoi(densityString)
+		chunkNumber, err1 := strconv.Atoi(chunkString)
+		concurrency, err2 := strconv.Atoi(concurrencyString)
+		density, err3 := strconv.Atoi(densityString)
+		voxelSize, err4 := strconv.ParseFloat(voxelSizeString, 64)
 
-		if err != nil || chunkNumber <= 0 || concurrency <= 2 || density <= 1 {
-			print("Error parsing chunk number, concurrency, or density. All must be integers (chunk number > 0, concurrency > 2, density > 1)")
+		if err1 != nil || err2 != nil || err3 != nil || err4 != nil || chunkNumber <= 0 || concurrency <= 2 || density <= 1 || voxelSize <= 0 {
+			print("Error parsing chunk number, concurrency, or density. All must be integers (chunk number > 0, concurrency > 2, density > 1, voxel size > 0)")
 			return
 		}
 	}
@@ -144,7 +148,7 @@ func main() {
 
 	go lasProcessing.CLIStatus(status, &quit, uiDone)
 
-	output := lasProcessing.ConcurrentProcess[voxels.DensityVoxelSet](file, chunks, &processor, concurrency, 0.1, status)
+	output := lasProcessing.ConcurrentProcess[voxels.DensityVoxelSet](file, chunks, &processor, concurrency, voxelSize, status)
 
 	quit = true
 
