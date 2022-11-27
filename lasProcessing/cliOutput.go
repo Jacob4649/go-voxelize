8707f1071736d4afbd7243dd1048584b372a6a46
Jacob Klimczak <jacob.klimczak@gmail.com>
2022-11-26 22:30:29 -0500

    add tab before merging

--- lasProcessing/cliOutput.go
+++ lasProcessing/cliOutput.go
@@ -31,7 +31,7 @@ func cliStatusPanel(status *ConcurrentStatus) {
 		if *progress != 1.0 {
 			println(ProgressBarFloat("P" + fmt.Sprint(i), *progress))
 		} else {
-			println("P" + fmt.Sprint(i) + ": Merging")
+			println("P" + fmt.Sprint(i) + ":\tMerging")
 		}
 	}
 }
