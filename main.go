bca561424e564306e6ab161803d44e97190932d4
Jacob Klimczak <jacob.klimczak@gmail.com>
2022-11-30 15:42:52 -0500

    add output image with minimum heights

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"flag"
 	"os"
-	"strconv"
 
 	"github.com/Jacob4649/go-voxelize/go-voxelize/lasProcessing"
 	"github.com/Jacob4649/go-voxelize/go-voxelize/lidarioMod"
@@ -29,60 +29,48 @@ type executionArgs struct {
 
 	// voxel size to use
 	voxelSize float64
+
+	// whether to normalize
+	normalize bool
+
+	// whether to convert to a gradient
+	gradient bool
+
+	// where to output minimum heights
+	minimumImagePath string
 }
 
 // parses the specified arguments
-func parseArgs(args []string) executionArgs {
+func parseArgs() executionArgs {
+	
+	destName := flag.String("output", "output.csv", "the file to output results to")
 
-	if len(args) < 3 || len(args) > 3 && len(args) < 7 {
-		println("Usage: voxelize <input LAS path> <output CSV path> <optional chunk number> <optional concurrency> " +
-			"<optional density> <optional voxel size>")
-		println("Must specify chunk number, concurrency, density, and voxel size to use any")
-		println("Default chunk number: 256")
-		println("Default concurrency: 32")
-		println("Default density: 20")
-		println("Default voxel size: 0.1")
-		os.Exit(1)
+	concurrency := flag.Int("concurrency", 32, "how many concurrent reads and processes to use")
+
+	chunkNumber := flag.Int("chunks", 256, "how many chunks to split the file into")
+
+	density := flag.Int("density", 20, "point density in a voxel to be filled")
+
+	voxelSize := flag.Float64("voxel", 0.1, "side length for a voxel")
+
+	normalize := flag.Bool("normalize", false, "whether to normalize output")
+
+	gradient := flag.Bool("gradient", false, "whether to convert output to a height gradient")
+
+	minimumImagePath := flag.String("minimum-output", "", "file path to output the minimums image")
+
+	flag.Parse()
+
+	fileName := flag.Arg(0)
+
+	if fileName == "" {
+		print("must define an output file")
+		os.Exit(0)
 	}
 
-	fileName := args[1]
-
-	destName := args[2]
-
-	concurrency := 32
-
-	chunkNumber := 256
-
-	density := 20
-
-	voxelSize := 0.1
-
-	if len(args) >= 7 {
-		chunkString := args[3]
-		concurrencyString := args[4]
-		densityString := args[5]
-		voxelSizeString := args[6]
-
-		var err1 error
-		var err2 error
-		var err3 error
-		var err4 error
-
-		chunkNumber, err1 = strconv.Atoi(chunkString)
-		concurrency, err2 = strconv.Atoi(concurrencyString)
-		density, err3 = strconv.Atoi(densityString)
-		voxelSize, err4 = strconv.ParseFloat(voxelSizeString, 64)
-
-		if err1 != nil || err2 != nil || err3 != nil || err4 != nil || chunkNumber <= 0 || concurrency <= 2 ||
-			density <= 1 || voxelSize <= 0 {
-			print("Error parsing chunk number, concurrency, or density. " +
-				"All must be integers (chunk number > 0, concurrency > 2, density > 1, voxel size > 0)")
-			os.Exit(1)
-		}
-	}
-
-	return executionArgs{fileName: fileName, destName: destName, 
-		concurrency: concurrency, chunkNumber: chunkNumber, density: density, voxelSize: voxelSize}
+	return executionArgs{fileName: fileName, destName: *destName, 
+		concurrency: *concurrency, chunkNumber: *chunkNumber, density: *density, voxelSize: *voxelSize,
+		normalize: *normalize, gradient: *gradient, minimumImagePath: *minimumImagePath}
 }
 
 // performs the main processing of the LAS file
@@ -125,10 +113,52 @@ func postProcessing[I any, O any](voxels I, pipeline lasProcessing.PostProcessin
 	return err
 }
 
+/// selects a post processing pipeline to use
+func choosePipeline(config executionArgs) lasProcessing.PostProcessingPipeline[*voxels.DensityVoxelSet, error] {
+	var finalPipeline lasProcessing.PostProcessingPipeline[*voxels.DensityVoxelSet, error]
+	
+	var voxelPipeline lasProcessing.PostProcessingPipeline[*voxels.DensityVoxelSet, *voxels.VoxelSet] = 
+		&voxels.VoxelCondenser{Density: config.density}
+
+	outputMinimums := config.minimumImagePath != ""
+
+	minimumPipeline := &voxels.MinimumHeightFinder{
+		OuptutMinimums: outputMinimums,
+		OutputFile: config.minimumImagePath,
+	}
+
+	if config.normalize {
+		heightPipeline := lasProcessing.ChainPipeline[*voxels.DensityVoxelSet, *voxels.VoxelSet, *voxels.MinimumHeights](
+			voxelPipeline, minimumPipeline)
+	
+		voxelPipeline = lasProcessing.ChainPipeline[*voxels.DensityVoxelSet, *voxels.MinimumHeights, *voxels.VoxelSet](
+			heightPipeline, &voxels.LazyNormalizer{})
+	} else if outputMinimums {
+		heightPipeline := lasProcessing.ChainPipeline[*voxels.DensityVoxelSet, *voxels.VoxelSet, *voxels.MinimumHeights](
+			voxelPipeline, minimumPipeline)
+	
+		voxelPipeline = lasProcessing.ChainPipeline[*voxels.DensityVoxelSet, *voxels.MinimumHeights, *voxels.VoxelSet](
+			heightPipeline, &voxels.MinimumDegrouper{})
+	}
+
+	finalPipeline = lasProcessing.ChainPipeline[*voxels.DensityVoxelSet, *voxels.VoxelSet, error](
+		voxelPipeline, &voxels.VoxelFileWriter{FileName: config.destName})
+
+	if config.gradient {
+		gradientPipline := lasProcessing.ChainPipeline[*voxels.DensityVoxelSet, *voxels.VoxelSet, *voxels.HeightGradient](
+			voxelPipeline, &voxels.GradientProcessor{})
+
+		finalPipeline = lasProcessing.ChainPipeline[*voxels.DensityVoxelSet, *voxels.HeightGradient, error](
+			gradientPipline, &voxels.GradientFileWriter{FileName: config.destName})
+	}
+
+	return finalPipeline
+}
+
 // Main function
 func main() {
 
-	config := parseArgs(os.Args)
+	config := parseArgs()
 
 	file, err := lidarioMod.NewLasFile(config.fileName, "rh")
 
@@ -145,8 +175,7 @@ func main() {
 
 	// post processing
 
-	pipeline := lasProcessing.ChainPipeline[*voxels.DensityVoxelSet, *voxels.VoxelSet, error](
-		&voxels.VoxelCondenser{Density: config.density}, &voxels.VoxelFileWriter{FileName: config.destName})
+	pipeline := choosePipeline(config)
 
 	err = postProcessing(output, pipeline, config)
 
--- voxels/voxels.go
+++ voxels/voxels.go
@@ -2,6 +2,10 @@ package voxels
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"math"
 	"os"
 
 	"github.com/Jacob4649/go-voxelize/go-voxelize/lasProcessing"
@@ -47,12 +51,153 @@ type VoxelSet struct {
 	Voxels mapset.Set[Coordinate]
 }
 
+// A height gradient of voxels
+type HeightGradient struct {
+
+	// the height gradient map
+	Gradient map[int]int
+
+}
+
 // Converts a point to a voxel Coordinate
 func PointToCoordinate(x float64, minX float64, y float64, minY float64, z float64, minZ float64, voxelSize float64) Coordinate {
 	
 	deltaX, deltaY, deltaZ := x - minX, y - minY, z - minZ
 	
 	return Coordinate{X: int(deltaX / voxelSize), Y: int(deltaY / voxelSize), Z: int(deltaZ / voxelSize)}
+}
+
+// The minimum heights at different coordinates
+type MinimumHeights struct {
+
+	// minimum heights
+	Heights map[XYPair]int
+
+	// voxels to use
+	Voxels *VoxelSet
+
+}
+
+// Finds minimum heights in each column
+type MinimumHeightFinder struct {
+
+	// whether to output minimums
+	OuptutMinimums bool
+
+	// filename to output to
+	OutputFile string
+
+}
+
+// hsvToRGB takes a color in HSV space with values hue(0.0 - 360.0),
+// saturation (0 - 1.0) and value (0-1.0) and returns its representation
+// in RGB color space, with values 0 - 0xFF.
+// FROM github.com/redbo/gohsv/
+func hsvToRGB(h, s, v float64) (r, g, b uint8) {
+	h, f := math.Modf(h / 60.0)
+	p := uint8(math.Round((v * (1.0 - s)) * 0xff))
+	q := uint8(math.Round((v * (1.0 - (s * f))) * 0xff))
+	t := uint8(math.Round((v * (1.0 - (s * (1.0 - f)))) * 0xff))
+	vr := uint8(math.Round(v * 0xff))
+	switch int(h) {
+	default:
+		return vr, t, p
+	case 1:
+		return q, vr, p
+	case 2:
+		return p, vr, t
+	case 3:
+		return p, q, vr
+	case 4:
+		return t, p, vr
+	case 5:
+		return vr, p, q
+	}
+}
+
+// writes minimum heights to an image
+func writeMinimumHeights(filename string, heights *MinimumHeights, status *lasProcessing.PipelineStatus, voxels *VoxelSet) {
+	
+	image := image.NewRGBA(image.Rect(0, 0, voxels.XVoxels, voxels.YVoxels))
+
+	total := len(heights.Heights)
+
+	current := 0
+	
+	for point, min := range heights.Heights {
+
+		// hsv colors
+		i := 200 + float64(min) / float64(voxels.ZVoxels) * -200
+		r, g, b := hsvToRGB(i, 1, 1)
+		color := color.RGBA{R: r, G: g, B: b, A: 255}
+
+		// greyscale colors
+		// i := 255 - uint8(float64(min) / float64(voxels.ZVoxels) * 255)
+		// color := color.RGBA{R: i, G: i, B: i, A: 255}
+		
+		image.SetRGBA(point.X, point.Y, color)
+
+		current += 1
+		*status = lasProcessing.PipelineStatus{Step: "Write min", Progress: float64(current) / float64(total)}
+	}
+
+	outputFile, err := os.Create(filename)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer outputFile.Close()
+
+	png.Encode(outputFile, image)
+}
+
+// finds minimum heights
+func(heightFinder *MinimumHeightFinder) Process(voxelSet *VoxelSet, status *lasProcessing.PipelineStatus) *MinimumHeights {
+
+	// map xy to minimum z value
+	minHeights := make(map[XYPair]int)
+
+	total := voxelSet.Voxels.Cardinality()
+
+	current := 0
+
+	*status = lasProcessing.PipelineStatus{Step: "Minimums", Progress: 0.0}
+
+	for voxel := range voxelSet.Voxels.Iterator().C {
+		xy := XYPair{X: voxel.X, Y: voxel.Y}
+		min, contains := minHeights[xy]
+		if contains {
+			if voxel.Z < min {
+				minHeights[xy] = voxel.Z
+			}
+		} else {
+			minHeights[xy] = voxel.Z
+		}
+
+		current += 1
+		*status = lasProcessing.PipelineStatus{Step: "Minimums", Progress: float64(current) / float64(total)}
+	}
+
+	heights := &MinimumHeights{Voxels: voxelSet, Heights: minHeights}
+
+	*status = lasProcessing.PipelineStatus{Step: "Write min", Progress: 0.0}
+
+	if heightFinder.OuptutMinimums {
+		writeMinimumHeights(heightFinder.OutputFile, heights, status, voxelSet)
+	}
+
+	return heights
+}
+
+// Turns minimum heights back into plain voxels
+type MinimumDegrouper struct {
+
+}
+
+// de groups minimum heights and voxels
+func(degrouper *MinimumDegrouper) Process(heights *MinimumHeights, status *lasProcessing.PipelineStatus) *VoxelSet {
+	return heights.Voxels
 }
 
 // Normalizes heights lazily
@@ -71,41 +216,19 @@ type XYPair struct {
 }
 
 // normalizes heights after the fact (z is up)
-func(normalizer *LazyNormalizer) Process(voxelSet *VoxelSet, status *lasProcessing.PipelineStatus) *VoxelSet {
-
-	// map xy to minimum z value
-	minHeights := make(map[XYPair]int)
-
-	total := voxelSet.Voxels.Cardinality()
-
-	current := 0
-
-	*status = lasProcessing.PipelineStatus{Step: "Finding minimums", Progress: 0.0}
-
-	for voxel := range voxelSet.Voxels.Iterator().C {
-		xy := XYPair{X: voxel.X, Y: voxel.Y}
-		min, contains := minHeights[xy]
-		if contains {
-			if voxel.Z < min {
-				minHeights[xy] = voxel.Z
-			}
-		} else {
-			minHeights[xy] = voxel.Z
-		}
-
-		current += 1
-		*status = lasProcessing.PipelineStatus{Step: "Finding minimums", Progress: float64(current) / float64(total)}
-	}
+func(normalizer *LazyNormalizer) Process(voxelSet *MinimumHeights, status *lasProcessing.PipelineStatus) *VoxelSet {
 
 	newVoxelSet := mapset.NewThreadUnsafeSet[Coordinate]()
 
-	current = 0
+	total := voxelSet.Voxels.Voxels.Cardinality()
+
+	current := 0
 
 	*status = lasProcessing.PipelineStatus{Step: "Normalizing", Progress: 0.0}
 
-	for voxel := range voxelSet.Voxels.Iterator().C {
+	for voxel := range voxelSet.Voxels.Voxels.Iterator().C {
 		xy := XYPair{X: voxel.X, Y: voxel.Y}
-		min := minHeights[xy]
+		min := voxelSet.Heights[xy]
 		
 		voxel.Z -= min
 		newVoxelSet.Add(voxel)
@@ -114,10 +237,44 @@ func(normalizer *LazyNormalizer) Process(voxelSet *VoxelSet, status *lasProcessi
 		*status = lasProcessing.PipelineStatus{Step: "Normalizing", Progress: float64(current) / float64(total)}
 	}
 
-	voxelSet.Voxels = newVoxelSet
+	voxelSet.Voxels.Voxels = newVoxelSet
 	
-	return voxelSet
+	return voxelSet.Voxels
 }
+
+// Converts voxels to a height gradient
+type GradientProcessor struct {
+
+}
+
+// finds minimum heights
+func(gradientProcessor *GradientProcessor) Process(voxelSet *VoxelSet, status *lasProcessing.PipelineStatus) *HeightGradient {
+
+	// map height to minimum coxel count
+	gradient := make(map[int]int)
+
+	total := voxelSet.Voxels.Cardinality()
+
+	current := 0
+
+	*status = lasProcessing.PipelineStatus{Step: "Gradient", Progress: 0.0}
+
+	for voxel := range voxelSet.Voxels.Iterator().C {
+		count, contains := gradient[voxel.Z]
+		
+		if contains {
+			gradient[voxel.Z] = count + 1
+		} else {
+			gradient[voxel.Z] = 1
+		}
+
+		current += 1
+		*status = lasProcessing.PipelineStatus{Step: "Gradient", Progress: float64(current) / float64(total)}
+	}
+
+	return &HeightGradient{Gradient: gradient}
+}
+
 
 // Writes the output to a file
 type VoxelFileWriter struct {
@@ -147,6 +304,44 @@ func(writer *VoxelFileWriter) Process(voxels *VoxelSet, status *lasProcessing.Pi
 
 	for voxel := range voxels.Voxels.Iterator().C {
 		_, err = file.WriteString(fmt.Sprint(voxel.X) + "," +fmt.Sprint(voxel.Y) + "," + fmt.Sprint(voxel.Z) + "\n")
+
+		if err != nil {
+			return err
+		}
+
+		current += 1
+		*status = lasProcessing.PipelineStatus{Step: "Writing", Progress: float64(current) / float64(total)}
+	}
+
+	return nil
+}
+
+// Writes a gradient to a file
+type GradientFileWriter struct {
+	// The file name to write to
+	FileName string
+}
+
+// Writes a set of voxels to a file
+func(writer *GradientFileWriter) Process(gradient *HeightGradient, status *lasProcessing.PipelineStatus) error {
+	file, err := os.Create(writer.FileName)
+
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	file.WriteString("height,count\n")
+
+	total := len(gradient.Gradient)
+
+	current := 0
+
+	*status = lasProcessing.PipelineStatus{Step: "Writing", Progress: 0.0}
+
+	for height, count := range gradient.Gradient {
+		_, err = file.WriteString(fmt.Sprint(height) + "," + fmt.Sprint(count) + "\n")
 
 		if err != nil {
 			return err
